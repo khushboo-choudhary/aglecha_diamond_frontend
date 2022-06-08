8e521d1f3b538275925a38696d4c612ec3481f48
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2022-06-08 13:27:48 +0530

    try to add search bar functionality

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Navbar.css"
 import { useNavigate, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,8 +8,13 @@ import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Avatar from '@mui/material/Avatar';
 import Chip from '@mui/material/Chip';
-import Tooltip,{tooltipClasses} from '@mui/material/Tooltip';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import { logout } from '../../Redux/LoginUserData/Action';
+// import axios from 'axios';
+// import { HashLoader } from "react-spinners";
+import { getData } from '../../Redux/CategoryData/Action';
+// import jsonData from "db.json";
+
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   '& .MuiBadge-badge': {
@@ -35,6 +40,7 @@ export default function Navbar() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const { id } = useParams()
+  console.log("vadai", id)
   const cart = useSelector((store) => store.cart.cart)
   const wishlist = useSelector((store) => store.cart.wishlist)
   console.log(wishlist)
@@ -43,47 +49,101 @@ export default function Navbar() {
   const isAuth = useSelector((store) => store.loginUserData.isAuthenticate)
   console.log("nav details ", userLogData)
 
+  // const [searchTerm, setSearchTerm] = useState([]);
+  // console.log(searchTerm);
+
+  //   useEffect(() => {
+  //     axios.get("https://diamond-khushboo.herokuapp.com/product").then((res) => setSearchTerm(res.searchTerm));
+  // })
+
+  const data = useSelector((store) => store.categoryReducer.categoryData[0])
+  const loading = useSelector((store) => store.categoryReducer.loading)
+  console.log("data coming from redux", data)
+  console.log("loading with spinner", loading)
+
+  useEffect(() => {
+
+    const dataSend = {
+      id,
+    }
+    dispatch(getData(dataSend))
+
+  }, [id, dispatch])
   return (
     <div>
       <div className='NavBar'>
         <div id="box" onClick={() => navigate("/")}><img src="logodiamonds.jpg" alt="" id="logo" /><p className='FontStyleName'>Aglecha Diamond</p></div>
-    
-      <div className='cursar' onClick={() => navigate("/category/earings/products")}>Earings</div>
-      <div className='cursar' onClick={() => navigate("/category/bracelets/products")}>Bracelets</div>
-      <div  className='cursar' onClick={() => navigate("/category/chains/products")}>Chains</div>
-      <div className='cursar' onClick={() => navigate("/category/rings/products")}>Rings</div>
-      <div className='cursar' onClick={() => navigate("/contact_us")}>Contact</div>
-      <div onClick={() => navigate("/login")}><input type="text" placeholder='Search' onClick={()=>navigate(`/${id}`)}/></div>
-       
-      {
-        isAuth === true ? <div className='avtar dropdown' >
-          <div className='name'><p>{userLogData[0].user.email[0]}</p></div>
-          <div className='dropdownInside'>
-            <a href="/">Hello, {userLogData[0].user.name}</a>
-            <a href="/" onClick={() => dispatch(logout())}>Logout</a>
 
-          </div>
-        </div> :
-          <div>
-            <div class="dropdown">
-              <div className='name' onClick={() => navigate("/register")}> <Chip 
-                avatar={<Avatar alt="Avatar" src="/broken-image.jpg" />}
-                label="Login"
-                variant="outlined"
-              /></div>
+        <div className='cursar' onClick={() => navigate("/category/earings/products")}>Earings</div>
+        <div className='cursar' onClick={() => navigate("/category/bracelets/products")}>Bracelets</div>
+        <div className='cursar' onClick={() => navigate("/category/chains/products")}>Chains</div>
+        <div className='cursar' onClick={() => navigate("/category/rings/products")}>Rings</div>
+        <div className='cursar' onClick={() => navigate("/contact_us")}>Contact</div>
+        <div><input type="text" placeholder='Search' onChange={(event) => navigate(`/${event.target.id}`)} /></div>
+
+        {/* <div className='ProductsDiv'> */}
+
+        {/* { */}
+          {/* data && loading === false ? */}
+            <>
+              {
+                data.map((e) => (
+                  <>
+                  <div className='IndividualProd' onClick={() => { navigate(`/${id}/${e._id}`) }}>
+                    <div className='IndividualProdImg'>
+                      <img src={e.image} alt="" />
+                    </div>
+                    {/* <button className="IndividualProdRatings">{e.customer_rating}</button> */}
+                    <div className='IndividualProdTitle'>
+
+                      <p>{e.description}</p>
+
+                      <p>
+                        <span>₹ {e.price.sp}</span>
+                        <span>₹ {e.price.mrp}</span>
+                        <button className="IndividualProdRatings">{e.customer_rating}</button>
+                      </p>
+                      <button className='IndividualProdBuyNow'>BUY NOW</button>
+                    </div>
+                  </div>
+                  </>
+                ))
+              }
+            </>
+        
+            {/* } */}
+
+        {/* </div> */}
+
+        {
+          isAuth === true ? <div className='avtar dropdown' >
+            <div className='name'><p>{userLogData[0].user.email[0]}</p></div>
+            <div className='dropdownInside'>
+              <a href="/">Hello, {userLogData[0].user.name}</a>
+              <a href="/" onClick={() => dispatch(logout())}>Logout</a>
 
             </div>
-          </div>
-      }
+          </div> :
+            <div>
+              <div class="dropdown">
+                <div className='name' onClick={() => navigate("/register")}> <Chip
+                  avatar={<Avatar alt="Avatar" src="/broken-image.jpg" />}
+                  label="Login"
+                  variant="outlined"
+                /></div>
 
-      <div onClick={() => navigate("/cart")}>
-        <IconButton aria-label="cart" >
-          <StyledBadge badgeContent={cart.length} color="primary">
-          <Toolstip title="Cart" color ="error" fontSize="40px"><IconButton > <ShoppingCartIcon /></IconButton></Toolstip>
-          </StyledBadge>
-        </IconButton>
+              </div>
+            </div>
+        }
+
+        <div onClick={() => navigate("/cart")}>
+          <IconButton aria-label="cart" >
+            <StyledBadge badgeContent={cart.length} color="primary">
+              <Toolstip title="Cart" color="error" fontSize="40px"><IconButton > <ShoppingCartIcon /></IconButton></Toolstip>
+            </StyledBadge>
+          </IconButton>
+        </div>
       </div>
-    </div>
     </div >
   )
 }
