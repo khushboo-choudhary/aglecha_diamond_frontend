6748443a053b42d10ee344d244ed63a6d3a2e632
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-06-26 23:59:25 +0530

    url changed

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect(process.env.MONGODB_URL);
+  return mongoose.connect(
+    "mongodb://khushboo:khushboo@ac-txtrrgk-shard-00-00.t4z9g8v.mongodb.net:27017,ac-txtrrgk-shard-00-01.t4z9g8v.mongodb.net:27017,ac-txtrrgk-shard-00-02.t4z9g8v.mongodb.net:27017/diamond?replicaSet=atlas-22qg4h-shard-0&ssl=true&authSource=admin"
+  );
 };
-
-
