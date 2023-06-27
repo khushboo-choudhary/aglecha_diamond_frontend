ec5e47f0560bf3856b6d48964910577db462ea6e
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-06-27 23:51:42 +0530

    url changed

--- Backend/src/configs/google-oauth.js
+++ Backend/src/configs/google-oauth.js
@@ -7,14 +7,15 @@ const User = require("../models/user.model");
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://diamond-khushboo.herokuapp.com/auth/google/callback",
-      
+      clientID:
+        "333875924404-3miq3l69dc76r7qpjagvmlpduor0l8j8.apps.googleusercontent.com", // process.env.GOOGLE_CLIENT_ID,
+      clientSecret: "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",
+      // process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:2345/auth/google/callback",
     },
     async function (request, accessToken, refreshToken, profile, done) {
-      console.log("user", request,accessToken,refreshToken);
-      console.log("profile" , profile)
+      // console.log("user", request,accessToken,refreshToken);
+      // console.log("profile" , profile)
       let user = await User.findOne({ email: profile?.email }).lean().exec();
 
       if (!user) {
@@ -24,11 +25,10 @@ passport.use(
           password: v4(),
           nickName: nickName,
           profileImage: profile?.photos[0].value,
-          
         });
-        console.log(nickName);
+        // console.log(nickName);
       }
-     console.log(nickName);
+      //  console.log(nickName);
       return done(null, user);
     }
   )
