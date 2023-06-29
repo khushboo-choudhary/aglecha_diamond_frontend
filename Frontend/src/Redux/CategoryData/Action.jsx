750929e576452dff7cc9cbebaf58cbc21403f141
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-06-29 23:14:15 +0530

    url changed.

--- Frontend/src/Redux/CategoryData/Action.jsx
+++ Frontend/src/Redux/CategoryData/Action.jsx
@@ -1,35 +1,33 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const DATA_LOADING = "DATA_LOADING";
 export const DATA_SUCCESS = "DATA_SUCCESS";
 export const DATA_FAILURE = "DATA_FAILURE";
- 
+
 export const dataLoading = () => ({
-    type : DATA_LOADING
+  type: DATA_LOADING,
 });
 
 export const dataSuccess = (payload) => ({
-    type : DATA_SUCCESS,
-    payload
+  type: DATA_SUCCESS,
+  payload,
 });
 
-export const dataFailure =() => ({
-    type: DATA_FAILURE
-})
+export const dataFailure = () => ({
+  type: DATA_FAILURE,
+});
 
-
-
-export const getData = (data) => (dispatch)=> {
-    dispatch(dataLoading())
-axios.get(`https://diamond-server-backend.herokuapp.com/product/${data.id}`, {
+export const getData = (data) => (dispatch) => {
+  dispatch(dataLoading());
+  axios
+    .get(`http://localhost:2345/product/${data.id}`, {
       params: {
-        sorting: data.sortby,   
+        sorting: data.sortby,
         sizes_like: data.size,
-        'discount': data.discount,
-        rating: data.rating
-      }
+        discount: data.discount,
+        rating: data.rating,
+      },
     })
     .then((res) => dispatch(dataSuccess(res.data)))
-    .catch((err) => dispatch(dataFailure()))
-  
-}
+    .catch((err) => dispatch(dataFailure()));
+};
--- Frontend/src/Redux/LoginUserData/Action.jsx
+++ Frontend/src/Redux/LoginUserData/Action.jsx
@@ -1,45 +1,56 @@
-import axios from 'axios'
+import axios from "axios";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const LOGIN_LOADING = "LOGIN_LOADING";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
-export const LOGOUT = "LOGOUT"
- 
+export const LOGOUT = "LOGOUT";
+
 export const loginLoading = () => ({
-    type : LOGIN_LOADING
+  type: LOGIN_LOADING,
 });
 
 export const loginSuccess = (payload) => ({
-    type : LOGIN_SUCCESS,
-    payload
+  type: LOGIN_SUCCESS,
+  payload,
 });
 
-export const loginFailure =() => ({
-    type: LOGIN_FAILURE
-})
+export const loginFailure = () => ({
+  type: LOGIN_FAILURE,
+});
 
 export const logout = () => ({
-  type : LOGOUT
-})
+  type: LOGOUT,
+});
 
+export const login = (data) => (dispatch) => {
+  dispatch(loginLoading());
 
+  axios
+    .post("http://localhost:2345/login", data)
+    .then((res) => {
+      dispatch(loginSuccess(res.data));
+      toast.success = "Login Success";
+    })
+    .catch((error) => {
+      alert(error.response.data.message);
+      dispatch(loginFailure());
+    });
+};
 
-export const login = (data) => (dispatch)=> {
-
-    dispatch(loginLoading())
-
-axios.post("https://diamond-server-backend.herokuapp.com/login", data).then((res) => {dispatch(loginSuccess(res.data)) ;  toast.success="Login Success"}).catch((error) => {alert(error.response.data.message); dispatch(loginFailure())})
-  
-}
-
-
-export const register = (data) => (dispatch)=> {
-
-    dispatch(loginLoading())
-    axios.post("https://diamond-server-backend.herokuapp.com/register", data).then((res) => {dispatch(loginSuccess(res.data)) ;toast.success="Register Successfully"}).catch((error) => {console.log(error.response.data.message) ; dispatch(loginFailure()) })
-
-}
-<ToastContainer/>
+export const register = (data) => (dispatch) => {
+  dispatch(loginLoading());
+  axios
+    .post("http://localhost:2345/register", data)
+    .then((res) => {
+      dispatch(loginSuccess(res.data));
+      toast.success = "Register Successfully";
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+      dispatch(loginFailure());
+    });
+};
+<ToastContainer />;
