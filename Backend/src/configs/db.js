a3174da578046fb1871cf74bb518829f9efefe69
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-09-11 12:35:24 +0530

    Folder name andfile path changed.

--- Backend/src/configs/db.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-
-module.exports = () => {
-  return mongoose.connect(
-    "mongodb://khushboo:khushboo@ac-txtrrgk-shard-00-00.t4z9g8v.mongodb.net:27017,ac-txtrrgk-shard-00-01.t4z9g8v.mongodb.net:27017,ac-txtrrgk-shard-00-02.t4z9g8v.mongodb.net:27017/diamond?replicaSet=atlas-22qg4h-shard-0&ssl=true&authSource=admin"
-  );
-};
